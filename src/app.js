89dcebfc319da87719244e8f97de856afade6173
Gmarques023 <153223582+Gmarques023@users.noreply.github.com>
2024-02-13 00:09:18 +0000

    random domain generators

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let topLevelDomains = [".com", ".org", ".net"];
+
+  let randomDomain = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let x = 0; x < topLevelDomains.length; x++) {
+          let domain = pronoun[i] + adj[j] + noun[k] + topLevelDomains[x];
+          randomDomain.push(domain);
+        }
+      }
+    }
+  }
+
+  console.log(randomDomain);
 };
